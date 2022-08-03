01de6bad7708c459aca98f170f2b42e49723cb6d
USER <USER@LAPTOP-4FTBNMOT>
2022-08-04 02:21:57 +0800

    初版

--- 配對遊戲/MainWindow.xaml.cs
+++ 配對遊戲/MainWindow.xaml.cs
@@ -15,16 +15,34 @@ using System.Windows.Shapes;
 
 namespace 配對遊戲
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
 
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            TimeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 18)
+            {
+                timer.Stop();
+                TimeTextBlock.Text = TimeTextBlock.Text + "-Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -35,33 +53,73 @@ namespace 配對遊戲
             List<string> matchword = new List<string>()
             {
                
-            "打","手",
-            "吃","口",
-            "英","艸",
-            "國","囗",
-            "他","人",
-            "江","水",
-            "板","木",
-            "快","心",
-            "好","女",
-            "煮","火",
-            "大","天",
-            "早","日",
-            "會","曰",
-            "有","月",
-            "次","欠",
-            "步","止",
-            "爭","爪",
-            "飯","食",
+            "打","打",
+            "吃","吃",
+            "英","英",
+            "國","國",
+            "他","他",
+            "江","江",
+            "板","板",
+            "快","快",
+            "好","好",
+            "煮","煮",
+            "天","天",
+            "早","早",
+            "會","會",
+            "有","有",
+            "次","次",
+            "步","步",
+            "爭","爭",
+            "飯","飯",
             };
             Random random = new Random();
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
-             {
-                int index = random.Next(matchword.Count);
-                string nextword = matchword[index];
-                textBlock.Text = nextword;
-                matchword.RemoveAt(index);
-             }
+            {
+                if (textBlock.Name != "TimeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(matchword.Count);
+                    string nextword = matchword[index];
+                    textBlock.Text = nextword;
+                    matchword.RemoveAt(index);
+                }
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+  
+                TextBlock textBlock = sender as TextBlock;
+                if (findingMatch == false)
+                {
+                    textBlock.Visibility = Visibility.Hidden;
+                    lastTextBlockClicked = textBlock;
+                    findingMatch = true;
+                }
+                else if (textBlock.Text == lastTextBlockClicked.Text)
+                {
+                    matchesFound++;
+                    textBlock.Visibility = Visibility.Hidden;
+                    findingMatch = false;
+                }
+                else
+                {
+                    lastTextBlockClicked.Visibility = Visibility.Visible;
+                    findingMatch = false;
+                }
+        }
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
         }
     }
 }
